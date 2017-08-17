b7689a56ea83576f2554fe9c45edaeeb7a7d3715
jianyufeng <907507643@qq.com>
2017-08-17 18:12:19 +0800

    New changelist范德萨

--- app/src/main/java/com/fule/mesurekeyheight/fragment/RefreshPractiveFragment.java
+++ app/src/main/java/com/fule/mesurekeyheight/fragment/RefreshPractiveFragment.java
@@ -1,14 +1,19 @@
 package com.fule.mesurekeyheight.fragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 import com.fule.mesurekeyheight.R;
+import com.fule.mesurekeyheight.activity.RepastPracticeActivity;
+import com.fule.mesurekeyheight.util.StatusBarUtil;
 
 /**
  * Created by Administrator on 2017/8/16.
@@ -24,6 +29,14 @@ public class RefreshPractiveFragment extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-//        StatusBarUtil.setPaddingSmart(getContext(), view.findViewById(R.id.toolbar));
+        StatusBarUtil.setPaddingSmart(getContext(), view.findViewById(R.id.toolbar));
+
+        ListView lv = (ListView) view.findViewById(R.id.recyclerView);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                startActivity(new Intent(getContext(),RepastPracticeActivity.class));
+            }
+        });
     }
 }
